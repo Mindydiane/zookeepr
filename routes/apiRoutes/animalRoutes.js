546244d9a999d362e08841ac5df56477108ec586
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-14 19:30:38 -0500

    zookeeper test and routes setup

--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -1,38 +1,43 @@
-const router = require('express').Router();
-const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
-const { animals } = require('../../data/animals');
+const router = require("express").Router();
+const {
+  filterByQuery,
+  findById,
+  createNewAnimal,
+  validateAnimal,
+} = require("../../lib/animals");
+const { animals } = require("../../data/animals");
 
 //add a route, get method requires two arguements
 router.get("/animals", (req, res) => {
-    let results = animals;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
-  
-  //get a new route for animals
-  router.get("/animals/:id", (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
-  });
-  
-  // post or create animals
-  router.post('/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-  
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
-    } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal);
-    }
-  });
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
 
-  module.exports  = router;
+//get a new route for animals
+router.get("/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+// post or create animals
+router.post("/animals", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
+module.exports = router;
